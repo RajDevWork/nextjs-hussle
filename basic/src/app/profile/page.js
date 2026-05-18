1724ffab043e7e43910994f893529c90834c15c9
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-18 19:07:01 +0530

    Working with static assets inside public

--- basic/src/app/profile/page.js
+++ basic/src/app/profile/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import  Image  from 'next/image';
 
 const page = () => {
   return (
-    <div>This is profile page.</div>
+    <div>This is profile page.
+
+        {/* <img src="Images/demo.jpg" alt="Profile Image" /> */}
+        <Image src="/Images/demo.jpg" alt="Profile Image" width={200} height={200} />
+    </div>
   )
 }
 
