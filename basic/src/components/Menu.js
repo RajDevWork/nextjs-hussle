9e26ff2dbd90a23f90b1005cac2c6ed0431a9682
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-20 18:43:14 +0530

    Link component prefetch and replace properties

--- basic/src/components/Menu.js
+++ basic/src/components/Menu.js
@@ -5,13 +5,16 @@ import React from 'react'
 
 const Menu = () => {
 
+    // replace: browser history me routes ke cache store ko avoid karta hain.
+    // prefetch: Browser page caching ko ignore karne ke liye hota hain.
+
     const currentPath = usePathname()
   return (
     <div className='flex gap-5 py-5'>
-        <Link href={'/'} className={currentPath==='/'?'active':''}>Home</Link>
-        <Link href={{pathname:'/about',query:{name:'RAM',price:'200'}}} className={currentPath==='/about'?'active':''}>About</Link>
-        <Link href={{pathname:'/admin',query:{name:'Raj',role:'admin'}}} className={currentPath==='/admin'?'active':''}>Admin</Link>
-        <Link href={'/contact'} className={currentPath==='/contact'?'active':''}>Contact</Link>
+        <Link href={'/'} className={currentPath==='/'?'active':''} replace>Home</Link>
+        <Link href={{pathname:'/about',query:{name:'RAM',price:'200'}}} className={currentPath==='/about'?'active':''} replace>About</Link>
+        <Link href={{pathname:'/admin',query:{name:'Raj',role:'admin'}}} className={currentPath==='/admin'?'active':''} replace>Admin</Link>
+        <Link prefetch={false} href={'/contact'} className={currentPath==='/contact'?'active':''} replace>Contact</Link>
     </div>
   )
 }
