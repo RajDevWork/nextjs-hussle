8e8344f38089ed8d490652134a649a1383394369
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-20 21:33:40 +0530

    understanding basepath and env variable

--- basic/src/app/page.js
+++ basic/src/app/page.js
@@ -1,25 +1,26 @@
-'use client'
+// 'use client'
 import Menu from '@/components/Menu';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
 
-  const router = useRouter()
-  const Goto = ()=>{
-    // router.push("/about")
-    // router.replace("/about")
-    // router.refresh() // for page refresh
-    // router.prefetch("/about")
-    // router.back();
-    router.forward()
-  }
+  // const router = useRouter()
+  // const Goto = ()=>{
+  //   // router.push("/about")
+  //   // router.replace("/about")
+  //   // router.refresh() // for page refresh
+  //   // router.prefetch("/about")
+  //   // router.back();
+  //   router.forward()
+  // }
 
   return (
     <div>
       <Menu />
       <h1 className='text-3xl font-bold text-gray-200'>This is HOME Page.</h1>
-      <button onClick={Goto}>Click here</button>
+      {/* <button onClick={Goto}>Click here</button> */}
+      {process.env.API_KEY}
     </div>
   );
 };
