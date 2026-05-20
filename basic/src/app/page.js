54affdfb103267110c075248c8a2c94046e00ca9
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-20 18:59:49 +0530

    Add forward navigation functionality to the Goto function

--- basic/src/app/page.js
+++ basic/src/app/page.js
@@ -1,11 +1,25 @@
+'use client'
 import Menu from '@/components/Menu';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+
+  const router = useRouter()
+  const Goto = ()=>{
+    // router.push("/about")
+    // router.replace("/about")
+    // router.refresh() // for page refresh
+    // router.prefetch("/about")
+    // router.back();
+    router.forward()
+  }
+
   return (
     <div>
       <Menu />
       <h1 className='text-3xl font-bold text-gray-200'>This is HOME Page.</h1>
+      <button onClick={Goto}>Click here</button>
     </div>
   );
 };
