fba97bb28aac5696c1384eac51f4a04dc97de094
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-20 21:45:19 +0530

    Understanding header config

--- basic/next.config.mjs
+++ basic/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  basePath:'/dev'
+  basePath:'/dev',
+  async headers(){
+    return [
+      {
+        source:'/about',headers:[{key:'app-auth-key',value:'raj1234rn'}]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
