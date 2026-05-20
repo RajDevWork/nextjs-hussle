341b6da1aff701ce7fa1804a05302b2fae5f1766
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-20 17:56:33 +0530

    Managing active link

--- basic/src/components/Menu.js
+++ basic/src/components/Menu.js
@@ -1,13 +1,17 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Menu = () => {
+
+    const currentPath = usePathname()
   return (
     <div className='flex gap-5 py-5'>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/about'}>About</Link>
-        <Link href={'/admin'}>Admin</Link>
-        <Link href={'/contact'}>Contact</Link>
+        <Link href={'/'} className={currentPath==='/'?'active':''}>Home</Link>
+        <Link href={'/about'} className={currentPath==='/about'?'active':''}>About</Link>
+        <Link href={'/admin'} className={currentPath==='/admin'?'active':''}>Admin</Link>
+        <Link href={'/contact'} className={currentPath==='/contact'?'active':''}>Contact</Link>
     </div>
   )
 }
