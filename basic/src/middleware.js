713c81e17e938c090e16110222628992a7d0b03b
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-21 21:36:25 +0530

    Middleware setup part-2

--- basic/src/middleware.js
+++ basic/src/middleware.js
@@ -4,12 +4,16 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
 //   return NextResponse.redirect(new URL('/home', request.url))
 console.log("Hello middleware running...")
-return NextResponse.json({
-    hello:'Hi i am middleware'
-})
+// return NextResponse.json({
+//     hello:'Hi i am middleware'
+// })
+// if(request.url.includes("admin")){
+    return NextResponse.redirect(new URL('/', request.url))
+// }
+// return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: '/about/:path*',
-// }
+export const config = {
+  matcher: '/about',
+}
