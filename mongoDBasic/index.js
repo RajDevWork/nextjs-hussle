06807bb4fa4c93106c8a2905bda65efda166ff1a
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-05-23 16:44:24 +0530

    Schema and Model

--- mongoDBasic/index.js
+++ mongoDBasic/index.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/nextmongo").then(()=>console.log("Connected to DB")).catch((err)=>console.log(err))
+
+
+// creating model and schema
+const UserSchema = new mongoose.Schema({
+    name:String,
+    age:Number,
+    isMarried:Boolean,
+    salary:Number,
+    gender:String
+})
+
+const User = mongoose.model("User",UserSchema)
