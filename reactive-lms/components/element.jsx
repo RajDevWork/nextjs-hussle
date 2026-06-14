a2db74ed76329d8ffc0f1c7274c29ae9f4278ccf
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-14 11:11:47 +0530

    Add new SVG assets for improved UI elements
    - Added next.svg for navigation purposes.
    - Added star.svg for rating features.
    - Added vercel.svg for branding.
    - Added window.svg for window representation in UI.

---
+++ reactive-lms/components/element.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Element = ({children}) => {
+    return (
+        <h1>
+            {children}
+        </h1>
+    );
+};
+
+export default Element;
---
+++ reactive-lms/components/support.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Support = ({children}) => {
+    return (
+        <h1>
+            {children}
+        </h1>
+    );
+};
+
+export default Support;
