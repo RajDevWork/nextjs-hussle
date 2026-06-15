1cb9ccf041fa0dddecaf857b54ebaa6cfcaf4c25
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-15 16:18:47 +0530

    feat: refactor course filtering and sorting components for improved mobile experience

---
+++ reactive-lms/app/(main)/courses/_components/FilterCourseMobile.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+// for mobile sidebar
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Filter } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+const FilterCourseMobile = ({CATEGORY_OPTIONS,filter,PRICE_OPTIONS}) => {
+    return (
+        <div className="lg:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Filter className="h-6 w-6" />
+              </SheetTrigger>
+              <SheetContent side="left">
+                <SheetHeader>
+                  <SheetTitle className="text-left">Filter Courses</SheetTitle>
+                  <Accordion defaultValue={["categories"]} type="multiple">
+                    {/* Categories filter */}
+                    <AccordionItem value="categories">
+                      <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                        <span className="font-medium text-gray-900">
+                          Categories
+                        </span>
+                      </AccordionTrigger>
+
+                      <AccordionContent className="pt-6 animate-none">
+                        <ul className="space-y-4">
+                          {CATEGORY_OPTIONS.map((option, optionIdx) => (
+                            <li
+                              key={option.value}
+                              className="flex items-center"
+                            >
+                              <Checkbox
+                                type="checkbox"
+                                id={`category-${optionIdx}`}
+                                onCheckedChange={() => {
+                                  applyArrayFilter({
+                                    type: "categories",
+                                    value: option.value,
+                                  });
+                                }}
+                                checked={filter.categories.includes(
+                                  option.value
+                                )}
+                              />
+                              <label
+                                htmlFor={`category-${optionIdx}`}
+                                className="ml-3 text-sm text-gray-600 cursor-pointer"
+                              >
+                                {option.label}
+                              </label>
+                            </li>
+                          ))}
+                        </ul>
+                      </AccordionContent>
+                    </AccordionItem>
+                    {/* Price filter */}
+                    <AccordionItem value="price">
+                      <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                        <span className="font-medium text-gray-900">Price</span>
+                      </AccordionTrigger>
+
+                      <AccordionContent className="pt-6 animate-none">
+                        <ul className="space-y-4">
+                          {PRICE_OPTIONS.map((option, optionIdx) => (
+                            <li
+                              key={option.value}
+                              className="flex items-center"
+                            >
+                              <Checkbox
+                                type="checkbox"
+                                id={`price-${optionIdx}`}
+                                onCheckedChange={() => {
+                                  applyArrayFilter({
+                                    type: "price",
+                                    value: option.value,
+                                  });
+                                }}
+                                checked={filter.price.includes(option.value)}
+                              />
+                              <label
+                                htmlFor={`price-${optionIdx}`}
+                                className="ml-3 text-sm text-gray-600 cursor-pointer"
+                              >
+                                {option.label}
+                              </label>
+                            </li>
+                          ))}
+                        </ul>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+    );
+};
+
+export default FilterCourseMobile;
