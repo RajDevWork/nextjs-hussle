4a9fe8c302a36fff9c1cfed854bfda9d64e41e3d
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-16 10:26:51 +0530

    feat: update SingleCoursePage to use async params and enhance getCourseDetails function with additional population

--- reactive-lms/app/(main)/courses/[id]/page.js
+++ reactive-lms/app/(main)/courses/[id]/page.js
@@ -2,8 +2,14 @@ import CourseDetailsIntro from "./_components/CourseDetailsIntro";
 import CourseDetails from "./_components/CourseDetails";
 import Testimonials from "./_components/Testimonials";
 import RelatedCourses from "./_components/RelatedCourses";
+import { getCourseDetails } from "@/queries/courses";
 
-const SingleCoursePage = () => {
+const SingleCoursePage = async ({params}) => {
+  const {id} = await params; // version 14 and earlier me {params: {id}} working tha but version 15 me async ho gya hain.
+  const course = await getCourseDetails(id);
+  // console.log("params = ",id)
+  // console.log("course = ",course)
+
   return (
     <>
       <CourseDetailsIntro />
--- reactive-lms/queries/courses.js
+++ reactive-lms/queries/courses.js
@@ -4,7 +4,7 @@ import { Category } from "@/models/category.model";
 import { User } from "@/models/user.model";
 import { Testimonial } from "@/models/testimonial.model";
 import { Module } from "@/models/module.model";
-import { replaceMongoIdInArray } from "@/lib/convertData";
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
 export async function getCourseList(){
     const courses = await Course.find().select(["title","subtitle","thumbnail","price","category","instructor"]).populate({
         path:'category',
@@ -20,4 +20,27 @@ export async function getCourseList(){
         model: Module
     }).lean();
     return replaceMongoIdInArray(courses)
+}
+
+
+//Get single course details
+export async function getCourseDetails(id){
+    const course = await Course.findById(id).populate({
+        path:'category',
+        model: Category
+    }).populate({
+        path:'instructor',
+        model: User
+    }).populate({
+        path:'testimonials',
+        model: Testimonial,
+        populate:{
+            path:"user",
+            model:User
+        }
+    }).populate({
+        path:'modules',
+        model: Module
+    }).lean();
+    return replaceMongoIdInObject(course)
 }
