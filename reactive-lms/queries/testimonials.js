cbaf5defaa7fa441f82a2a976b03021a1714e5c9
Rajeshwar Parihar <138497997+RajDevWork@users.noreply.github.com>
2026-06-25 15:16:24 +0530

    Update testimonials.js

--- reactive-lms/queries/testimonials.js
+++ reactive-lms/queries/testimonials.js
@@ -1,6 +1,8 @@
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData"; 
 import { Testimonial } from "@/models/testimonial.model";
 
+
+/** Get the testimonials based on courseId **/
 export async function getTestimonialsForCourse(courseId){
     const testimonials = await Testimonial.find({courseId: courseId}).lean();
     return replaceMongoIdInArray(testimonials);
