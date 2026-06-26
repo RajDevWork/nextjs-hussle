4086e621785af8b98bca131a2475e69e5594400b
Rajeshwar Parihar <138497997+RajDevWork@users.noreply.github.com>
2026-06-26 08:53:34 +0530

    Update use-lock-body.js

--- reactive-lms/hooks/use-lock-body.js
+++ reactive-lms/hooks/use-lock-body.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 
 // @see https://usehooks.com/useLockBodyScroll.
+
+/** Lock screen when dialog model appears**/
 export function useLockBody() {
   React.useLayoutEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
