c2c6b25532b0ea54b4fda194b2b152a388fb5016
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-07-04 22:24:49 +0530

    Add reOrderModules function and integrate with ModulesForm for module reordering

--- reactive-lms/app/actions/module.js
+++ reactive-lms/app/actions/module.js
@@ -10,6 +10,7 @@
 "use server"
 
 import { Course } from "@/models/course.model";
+import { Module } from "@/models/module.model";
 import { create } from "@/queries/modules";
 
 export async function createModule(data){
@@ -37,4 +38,16 @@ export async function createModule(data){
     } catch (e) {
         throw new Error(e);
     }
+}
+
+export async function reOrderModules(data){
+
+    try {
+        await Promise.all(data.map(async(element) => {
+            await Module.findByIdAndUpdate(element.id, {order: element.position});
+        }));
+    } catch (e) {
+        throw new Error(e);
+    }
+
 }
