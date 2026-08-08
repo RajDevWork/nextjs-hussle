dfeabfdd3d19ea4272737b95ad4d855252545a17
Rajeshwar Parihar <138497997+RajDevWork@users.noreply.github.com>
2026-08-08 15:30:41 +0530

    Update mongo.js

--- reactive-lms/service/mongo.js
+++ reactive-lms/service/mongo.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose'
 
+
+/** * Establishes a connection to the MongoDB database. * * Uses the MongoDB connection URI stored in the environment variables. * Logs a success message when the connection is established and * logs the error if the connection attempt fails. */
+
 export async function ConnectToDB(){
     try {
         await mongoose.connect(String(process.env.MONGO_URI))
